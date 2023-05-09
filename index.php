8944cc57a0ecc8a25a6f1d469aefd9587905b300
Diana Calderon <dianacalderon@ravn.co>
2023-05-09 08:21:49 -0600

    Change path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
   switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-      if($_SERVER['REQUEST_URI'] === '/') {
+      if($_SERVER['REQUEST_URI'] === 'dogs') {
         get_dogs();
         break;
       }
