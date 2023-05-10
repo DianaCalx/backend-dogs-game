bc78f63563d5a93f72c9df8d30b1917a32f7ada6
Diana Calderon <dianacalderon@ravn.co>
2023-05-10 17:38:33 -0600

    Create variables file

---
+++ utils/variables.php
@@ -0,0 +1,8 @@
+<?php
+    //Routes
+    define('DOGS_ROUTE', '/rest/dogs');
+    define('DOG_ROUTE', '/rest/dog');
+    
+    //Global variables
+    define('BASE_API_URL', 'https://dog.ceo/api');
+?>
