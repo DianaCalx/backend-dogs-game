c6b26be23ba65d56924c87370c2311a6a8df7889
Diana Calderon <dianacalderon@ravn.co>
2023-05-10 17:39:38 -0600

    Clean dogs file logic

--- controllers/dogs.php
+++ controllers/dogs.php
@@ -1,30 +1,15 @@
 
 <?php
+    require_once('./utils/variables.php');
+    require_once('./utils/curl.php');
+
     function get_dogs() {
-        $ch = curl_init();
-
         $url = BASE_API_URL.'/breeds/list/all';
-
-        curl_setopt($ch,CURLOPT_URL,$url);
-        
-        curl_exec($ch);
-
-        if(curl_errno($ch)) echo curl_error($ch);
-
-        curl_close($ch);
+        get_curl($url);
     }
 
     function get_dog($breed) {
-        $ch = curl_init();
-
         $url = BASE_API_URL.'/breed/'.$breed.'/images/random';
-        
-        curl_setopt($ch,CURLOPT_URL, $url);
-        
-        curl_exec($ch);
-
-        if(curl_errno($ch)) echo curl_error($ch);
-
-        curl_close($ch);        
+        get_curl($url);    
     }
 ?>
